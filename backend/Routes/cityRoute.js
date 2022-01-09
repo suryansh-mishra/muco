1f743ee1a64394a93e98d43d151255d40c9363d0
Suryansh <95637226+suryansh-mishra@users.noreply.github.com>
2022-01-09 22:19:35 +0530

    Delete backend directory

--- backend/Routes/cityRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cityController = require("../Controllers/cityController.js");
-router
-  .route("/registeradmin")
-  .post(cityController.protect, cityController.registeradmin);
-router.route("/getcity").get(cityController.getCity);
-router.route("/getonecity/:id").get(cityController.getOneCity);
-module.exports = router;
